09589da64a0ca48515ac0063c4e4d4aeb4c7c103
Rahul Koduri <rahulkoduri96@gmail.com>
2024-06-27 00:13:44 +0530

    Added

---
+++ script.js
@@ -0,0 +1,11 @@
+let cart = document.querySelectorAll('.cart-btn');
+let n = cart.length;
+let span = document.getElementById('toast');
+let count=0;
+
+for(i=0; i<n; i++){
+   cart[i].addEventListener("click", function(){ 
+   ++count;
+   span.innerHTML = count;
+});
+}
